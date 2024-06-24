9d11084a1864b1d524ae340d94acb6c8bfd97c11
samueldlay <22435185+samueldlay@users.noreply.github.com>
2024-06-23 19:38:09 -0600

    add rename file feature

--- src/UploadFile.tsx
+++ src/UploadFile.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { ReactElement, useState } from "react";
 import { useAuth } from "./Authentication";
 import { getFiles, downloadFile, removeFile } from "./fetchAPI";
@@ -11,16 +11,52 @@ function truncate(str: string) {
   return `${trunc}${trunc.length >= 20 ? "(trunc)" : ""}.${extension}`;
 }
 
+function fileExtension(originalFile: string, newVal: string) {
+  const newValExt = newVal.split(".").pop();
+  const originalExtFile = originalFile.split(".").pop();
+  if (newValExt !== originalExtFile) {
+    return `${newVal}.${originalExtFile}`;
+  }
+  // const split = newVal.split(".");
+  // split.pop();
+  return newVal;
+}
+
 export default function UploadFile(): ReactElement {
   const auth = useAuth();
   const { files, setFiles } = getFiles("/api/files");
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [fileText, setFileText] = useState("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setSelectedFile(event.target.files[0]);
+      setFileText(event.target.files[0].name);
+      console.log(event.target.files)
     }
   };
+
+  const handleTextInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    // const newFileName = fileExtension(value)
+    // biome-ignore lint/style/noNonNullAssertion: <Biome is wrong>
+    const fileExt = fileExtension(selectedFile?.name!, value)
+    setFileText(fileExt);
+    // if (selectedFile?.name) selectedFile.name = value;
+    if (selectedFile) {
+      console.log("file type", selectedFile.type);
+    }
+  }
+
+  const changeFileName = () => {
+    if (selectedFile) {
+      const blob = new Blob([selectedFile], { type: selectedFile.type });
+      const file = new File([blob], fileText, { type: blob.type });
+      console.log("why is this empty?", file.type);
+      setSelectedFile(file)
+      console.log(file);
+    }
+  }
 
   const handleUpload = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
@@ -68,10 +104,25 @@ export default function UploadFile(): ReactElement {
   return (
     <div className="flex flex-col pb-20">
       <form className="flex flex-col items-center gap-4" onSubmit={handleUpload}>
-        <h2>Upload File</h2>
-        {/* <Input type="file" onChange={handleFileChange} /> */}
-        <label htmlFor="image_uploads">Choose images to upload (PNG, JPG)</label>
-        <input name="image_uploads" id="image_uploads" style={{ background: selectedFile ? "lawngreen" : "salmon" }} className="bg-red-400 rounded-lg" placeholder="test" type="file" onChange={handleFileChange} />
+        <input
+          type="file"
+          id="fileElem"
+          // multiple
+          // accept="image/*"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
+        {selectedFile ? <div>
+          <p>{selectedFile.name}</p>
+          <Input value={fileText} onChange={handleTextInputChange} type="text" />
+          <Button onClick={changeFileName}>Change filename</Button>
+        </div> : null}
+        <Button onClick={() => {
+          const fileElm = document.getElementById("fileElem");
+          fileElm?.click();
+        }} className="w-72" type="button">
+          Choose File
+        </Button>
         <Button className="w-72" type="submit">
           Upload
         </Button>
@@ -80,7 +131,7 @@ export default function UploadFile(): ReactElement {
         <h1 className="text-4xl">
           {files.length ? "Files:" : "No files in this directory"}
         </h1>
-        <ul className="flex flex-col gap-4">
+        <ul className="flex bg-slate-200 p-2 md:p-4 h-[40rem] overflow-scroll flex-col gap-4">
           {files?.map((file, index) => (
             <li className="flex text-xl flex-col md:flex-row gap-2" key={file}>
               {index + 1}.) {truncate(file)}
